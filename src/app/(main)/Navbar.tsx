d4c8a1e357baa535f68607e42b224527da7847ef
GedeonMatabaro <mugishomatgedeon@gmail.com>
2024-08-20 10:06:58 +0200

    project development

---
+++ src/app/(main)/Navbar.tsx
@@ -0,0 +1,23 @@
+
+import SearchField from "@/components/SearchField";
+import UserButton from "@/components/UserButton";
+import Link from "next/link";
+import MenuBar from "./MenuBar";
+
+export default function Navbar() {
+  return (
+    <header className="sticky top-0 z-10 bg-card shadow-sm">
+      <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-between gap-5 px-5 py-3">
+  <div className="flex items-center gap-5">
+    <Link href="/" className="text-2xl font-bold text-primary">
+      Globi
+    </Link>
+    <SearchField />
+  </div>
+  {/* <MenuBar className="hidden sm:flex sm:flex-1 sm:justify-center" />
+   */}
+  <UserButton className="ml-auto lg:ml-0" />
+</div>
+    </header>
+  );
+}
